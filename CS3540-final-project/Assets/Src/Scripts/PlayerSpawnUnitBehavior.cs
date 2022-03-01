6c8ca641005382ed13a4be92422aadbddfc823dd
Hongyi Zhang <zhang.hongyi@foxmail.com>
2022-02-28 20:02:08 -0500

    Add spawn unit feature

---
+++ CS3540-final-project/Assets/Src/Scripts/PlayerSpawnUnitBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnUnitBehavior : MonoBehaviour
+{
+    public GameObject unitPrefab;
+    public float spawnDistance = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnUnit(unitPrefab);
+        }
+    }
+
+    void SpawnUnit(GameObject unitToSpawn)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f))
+        {
+            GameObject unit = Instantiate(unitToSpawn, hit.point,  transform.parent.transform.rotation);
+        }
+    }
+}
