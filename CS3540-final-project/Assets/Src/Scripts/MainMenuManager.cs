f7fc97e24d99b81109f6659e4a9ac554c36b96ea
Terese Wang <wang.haoqi@husky.neu.edu>
2022-04-15 00:06:01 -0400

    add main menu

---
+++ CS3540-final-project/Assets/Src/Scripts/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
